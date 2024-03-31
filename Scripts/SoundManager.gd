89352035de6f460deed4c91bb8168b75ead98b85
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-03-31 14:58:21 -0400

    Progress on setting up randomizer

--- Scripts/SoundManager.gd
+++ Scripts/SoundManager.gd
@@ -3,22 +3,21 @@ extends Node
 var continueSound = preload("res://assets/audio/soundEffects/selectsymbolsound3.mp3")
 var returnSound = preload("res://assets/audio/soundEffects/selectsymbolsound.mp3")
 var switchSound = preload("res://assets/audio/soundEffects/selectsymbolsound2.mp3")
-var cardSelectSound = preload("res://assets/audio/soundEffects/cardSelect.mp3")
+var cardSelectSound = preload("res://assets/audio/soundEffects/cardSelectionSounds/cardselect4.mp3")
 
 var sortCards = preload("res://assets/audio/soundEffects/sortcards2.mp3")
-var pauseMenuOpen = preload("res://assets/audio/soundEffects/pausemenuopen2.mp3")
-var pauseMenuClose = preload("res://assets/audio/soundEffects/pausemenuclose2.mp3")
+var pauseMenuOpen = preload("res://assets/audio/soundEffects/pauseOpen.mp3")
+var pauseMenuClose = preload("res://assets/audio/soundEffects/pauseClose.mp3")
 var defeatSound = preload("res://assets/audio/soundEffects/luckydeckdefeat.mp3")
 var victorySound = preload("res://assets/audio/soundEffects/luckydeckvictory2.mp3")
 
-#	Here is the sounds With their Volume
 var soundDictionary = [[continueSound, -4], 
 [returnSound, -3], 
 [switchSound, -5], 
 [cardSelectSound, 4],
 [sortCards,-6],
 [pauseMenuOpen,-6],
-[pauseMenuClose,-11],
+[pauseMenuClose,-3],
 [defeatSound,-6],
 [victorySound,-6]]
 
@@ -32,6 +31,7 @@ PAUSE_CLOSE_SOUND,
 DEFEAT_SOUND,
 VICTORY_SOUND}
 
+
 #Only One Song Will Play at a time
 var currentSong : AudioStreamPlayer
 var nextSong : AudioStreamPlayer
@@ -44,6 +44,7 @@ func play_preset(soundKey: int):
 	instance.finished.connect(remove_node.bind(instance))
 	add_child(instance)
 	instance.play()
+
 
 func play_sound(stream: AudioStream):
 	var instance = AudioStreamPlayer.new()
